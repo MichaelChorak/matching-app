62c4612fbcc7058977ef2f6f943834f8304fb612
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-03-18 13:28:23 +0100

    Added model, connection db

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const ejs = require('ejs');
 const port = 3000;
 
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0u7k.mongodb.net/Cluster0?retryWrites=true&w=majority`
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true, dbName:process.env.DB_NAME});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
 
