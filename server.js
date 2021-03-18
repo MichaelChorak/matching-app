f7e4f22e95af02a442ed96419d42556acb956134
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-03-18 15:39:26 +0100

    added packages, added functionality, requires
    Added session and local, serialising, requiring, session use, dynamic port, requiring fields in html

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const app = express();
+const passport = require('passport');
+const mongoose = require('mongoose');
+const expressSession = require('express-session');
 const ejs = require('ejs');
-const port = 3000;
+const app = express();
+const port = process.env.PORT;
+
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0u7k.mongodb.net/Cluster0?retryWrites=true&w=majority`
 mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true, dbName:process.env.DB_NAME});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+ 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
 
