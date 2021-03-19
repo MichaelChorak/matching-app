d4074b7b8818b01feba7eb9255b738d309f660eb
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-03-19 12:18:24 +0100

    added name string in model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
  
 module.exports = mongoose.model('User',{
     username: String,
+    name: String,
     password: String,
-    email: String,
+    email: String
 });
