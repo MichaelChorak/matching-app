29f59a6d8b3f83d354975130ab7e0356810dfd04
soofg <sofya.gerges@hva.nl>
2021-03-20 21:11:13 +0100

    front-end & dishes from database

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 console.log('test');
 
 // imports
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const { MongoClient, ObjectID } = require('mongodb');
 const port = 3000;
 const ejs = require('ejs');
 
+
+// verbinden met de mongo database
+let db = null;
+// function connectDB
+async function connectDB() {
+  // get URI from .env file
+  const uri = process.env.DATABASECONNECT;
+  // make connection to database
+  const options = { useUnifiedTopology: true };
+  const client = new MongoClient(uri, options);
+  await client.connect();
+  db = await client.db(process.env.DATABASENAME);
+}
+
+// een 'test' om te achterhalen of de database is verbonden
+connectDB()
+  .then(() => {
+    // if succesfull connections is made, show a message
+    console.log('We have a connection to Mongo!');
+  })
+  .catch((error) => {
+    // if connnection is unsuccesful, show errors
+    console.log(error);
+  });
+
+// dit is waar de applicatie zich bevindt, hij 'luistert op port 3000, daar wordt de app weergegeven
 // Listen on port
 app.listen(3000, () => {
   console.log('Express web app on localhost:3000');
@@ -18,25 +46,33 @@ app.use(express.static('public/images'));
 app.use(express.static('public/js'));
 app.use('/css', express.static('/public/css')); // link naar je css folder
 app.use('/js', express.static('/public/js')); // link naar je js folder
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let dishes = null;
 
 app.get('/', (req, res) => {
-  res.render('index', { text: '' });
+    res.render('index', { text: '' });
 });
 
-app.get('/dishes', (req, res) => {
-    res.render('dishes', { text: '' });
-  });
 
-app.get('/dishes/:dishesId', (req, res) => {
-    res.render('dishesdetails', { text: '' });
-  });
+app.get('/thedishes', async (req, res) => {
+    let dishes = {}; 
+    dishes = await db.collection('dishes').find({}, { sort: {} }).toArray(); // data vanuit de database
+    res.render('thedishes', { text: '', dishes });
+ });
 
-app.get('/dishesdetails', (req, res) => {
-    res.render('dishesdetails', { text: '' });
-  });
+app.get('/thedishes/:dishesId', async (req, res) => { 
+  const dish = await db.collection('dishes').findOne({ id: req.params.dishesId });
+  res.render('dishesdetails', { title: 'Clothing Details', dish });
+});
+
+app.get('/', (req, res) => {
+    res.render('index', { text: '' });
+});
+
 
 app.get('/favoritedishes', (req, res) => {
     res.render('favoritedishes', { text: '' });
-  });
+});
 
 app.set('view engine', 'ejs');
