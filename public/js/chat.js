36532c8c2a228f2566649b46d725e7f5aef431d0
Timmhart <tim.hart@hva.nl>
2021-03-23 13:21:41 +0100

    added some files and made dynamic routes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,32 @@
-var socket = io();
+// Make connection
+var socket = io.connect('http://localhost:3000');
 
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit('chat message', input.value);
-    input.value = '';
-  }
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+  message.value = "";
 });
 
-socket.on('chat message', function(msg) {
-  var item = document.createElement('li');
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    feedback.innerHTML = "";
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' +data + ' is typing a message... </em></p>';
+})
