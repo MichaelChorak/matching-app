f9895e6a529cd86cacb2d18cb809b81e96f9cbc9
hoa0 <t-hoa.l@hotmail.com>
2021-03-25 14:03:41 +0100

    add dropMenu Setting

--- public/js/script.js
+++ public/js/script.js
@@ -22,6 +22,7 @@ function getRandomImage() {
 }
 
 /* settings menu microinteraction - profile page */
+/* https://stackoverflow.com/questions/64420332/javascript-how-to-create-a-dropdown-effect-without-jquery */
 var menuBtn = document.getElementById('settingBnt'); //b
 var dropdown = document.getElementsByClassName('showSettingsMenu')[0]; //ul
 menuBtn.addEventListener('click', getShowSettings);
