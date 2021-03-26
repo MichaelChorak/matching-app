a8559221aa90d6f009b6a53e2e5248d2dab2aaf9
Hoa0 <49392248+Hoa0@users.noreply.github.com>
2021-03-26 12:08:22 +0100

    Update script.js
    profile page

--- public/js/script.js
+++ public/js/script.js
@@ -22,3 +22,36 @@ function showSubmenu2() {
 headMenu.addEventListener('click', showWholeMenu);
 submenu1.addEventListener('click', showSubmenu1);
 submenu2.addEventListener('click', showSubmenu2);
+
+/* javascript: profile page, get a random image from an extern api */
+// https://codepen.io/FlorinPop17/details/vYYaLwR
+//https://www.youtube.com/watch?v=7f2HNadULOs
+const buttonRandomImage = document.getElementById('imageBtn');
+const displayImage = document.getElementById('showImage');
+
+buttonRandomImage.addEventListener('click', getRandomImage);
+
+/* funtion with the method fetch(), to get an random image*/
+function getRandomImage() {
+  fetch('https://random.dog/woof.json')
+    .then(res => res.json())
+    .then(data => {
+      if (data.url.includes('.mp4')) {
+        getRandomImage();
+      }
+      else {
+        displayImage.innerHTML = `<img src=${data.url} alt="randomImage" />`;
+      }
+    });
+}
+
+/* settings menu microinteraction - profile page */
+/* https://stackoverflow.com/questions/64420332/javascript-how-to-create-a-dropdown-effect-without-jquery */
+const menuBtn = document.getElementById('settingBnt'); //b
+const dropdown = document.getElementsByClassName('showSettingsMenu')[0]; //ul
+menuBtn.addEventListener('click', getShowSettings);
+
+function getShowSettings() {
+  this.classList.toggle("active");
+  dropdown.classList.toggle("active");
+}
