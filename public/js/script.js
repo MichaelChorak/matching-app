7cc2bfa55efbc2c93ac2193339cb0dda5fa5749b
soofg <sofya.gerges@hva.nl>
2021-03-26 10:08:50 +0100

    header aangepast + css

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 const   headMenu = document.querySelector('#headMenu'); // rondje
 const   wholeMenu = document.querySelector('#wholeMenu'); // dishes en persons
 const   submenu1  = document.querySelector('#submenu1'); //dishes menuknop / naam
-const   menuOne   = document.querySelector('#menuOne'); //dishes menu
+const   menuOne = document.querySelector('#menuOne'); //dishes menu
 const   submenu2  = document.querySelector('#submenu2'); //persons menuknop / naam
-const   menuSecond    = document.querySelector('#menuSecond'); // persons menu
+const   menuSecond  = document.querySelector('#menuSecond'); // persons menu
 
 function showWholeMenu() {
     wholeMenu.classList.toggle('showWholeMenu');
@@ -17,6 +17,10 @@ function showSubmenu1() {
 
 function showSubmenu2() {
     menuSecond.classList.toggle('showSubmenu2');
+  }
+
+function showWholeHamburgermenu() {
+    wholeHamburgermenu.classList.toggle('showWholeHamburgermenu');
   }
 
 headMenu.addEventListener('click', showWholeMenu);
