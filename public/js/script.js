fb745e8a444da170f8dedd84c63c95254fabf2dc
Hoa0 <49392248+Hoa0@users.noreply.github.com>
2021-03-26 12:30:59 +0100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,12 @@ const   menuOne   = document.querySelector('#menuOne');
 const   submenu2  = document.querySelector('#submenu2');
 const   menuSecond    = document.querySelector('#menuSecond');
 
+/* profile */
+const buttonRandomImage = document.getElementById('imageBtn');
+const displayImage = document.getElementById('showImage');
+const menuBtn = document.getElementById('settingBnt'); 
+const dropdown = document.getElementsByClassName('showSettingsMenu')[0];
+
 function showWholeMenu() {
     wholeMenu.classList.toggle('showWholeMenu');
     console.log('showWholeMenu');
@@ -19,18 +25,9 @@ function showSubmenu2() {
     menuSecond.classList.toggle('showSubmenu2');
   }
 
-headMenu.addEventListener('click', showWholeMenu);
-submenu1.addEventListener('click', showSubmenu1);
-submenu2.addEventListener('click', showSubmenu2);
-
 /* javascript: profile page, get a random image from an extern api */
 // https://codepen.io/FlorinPop17/details/vYYaLwR
 //https://www.youtube.com/watch?v=7f2HNadULOs
-const buttonRandomImage = document.getElementById('imageBtn');
-const displayImage = document.getElementById('showImage');
-
-buttonRandomImage.addEventListener('click', getRandomImage);
-
 /* funtion with the method fetch(), to get an random image*/
 function getRandomImage() {
   fetch('https://random.dog/woof.json')
@@ -47,11 +44,16 @@ function getRandomImage() {
 
 /* settings menu microinteraction - profile page */
 /* https://stackoverflow.com/questions/64420332/javascript-how-to-create-a-dropdown-effect-without-jquery */
-const menuBtn = document.getElementById('settingBnt'); //b
-const dropdown = document.getElementsByClassName('showSettingsMenu')[0]; //ul
-menuBtn.addEventListener('click', getShowSettings);
-
 function getShowSettings() {
   this.classList.toggle("active");
   dropdown.classList.toggle("active");
 }
+
+headMenu.addEventListener('click', showWholeMenu);
+submenu1.addEventListener('click', showSubmenu1);
+submenu2.addEventListener('click', showSubmenu2);
+
+buttonRandomImage.addEventListener('click', getRandomImage);
+menuBtn.addEventListener('click', getShowSettings);
+
+
