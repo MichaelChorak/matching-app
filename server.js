954850707c8bb7720d2b9e536ef7b14298558653
Timmhart <tim.hart@hva.nl>
2021-03-31 13:52:46 +0200

    fixed some routes and styles

--- server.js
+++ server.js
@@ -125,10 +125,7 @@ async function run() {
 run().catch(console.dir);
 
 
-// chatOverview route
-app.get("/chat", isAuthenticated, (req, res) => {
-  res.render("chat");
-});
+
 
 
 //route
@@ -489,7 +486,7 @@ app.post("/favoritedishes", async (req, res) => {
 });
 
 // chatOverview route
-app.get("/chat", (req, res) => {
+app.get("/chat", isAuthenticated, (req, res) => {
   res.render("chat");
 });
 
